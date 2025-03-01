e9529dddab3b152a247cfedbad76b5e88a42def5
MAC1713 <1172820376@qq.com>
2025-03-01 22:37:02 +0800

    Update function_7zip.py
    删除注释

--- module/function_7zip.py
+++ module/function_7zip.py
@@ -6,16 +6,12 @@ from constant import _PATH_7ZIP, _PASSWORD_FAKE, _COLOR_SKIP, _COLOR_ERROR, _COL
 
 
 def call_7zip(command_type: str, filepath: str, password: str, check_path_inside=None):
-    """调用7zip的l和t命令，返回测试结果
-    :param command_type: 7zip的command，l/t
-    :param filepath: 文件路径
-    :param password: 需要测试的密码
-    :param check_path_inside: 指定测试的内部文件路径（只在t指令时使用）"""
+
     command = [_PATH_7ZIP,
                command_type,
                filepath,
                "-p" + password]
-    if command_type == 't' and check_path_inside:  # 只在t指令使用，l指令不需要，t指令中使用时可以加快速度
+    if command_type == 't' and check_path_inside:  
         command.append(check_path_inside)
     print('测试 7zip命令', command)
     process = subprocess.run(command,
@@ -25,74 +21,44 @@ def call_7zip(command_type: str, filepath: str, password: str, check_path_inside
                              text=True,
                              universal_newlines=True)
 
-    """
-    7zip Exit Codes
-    0	没有错误
-    1	警告（非致命错误，例如被占用）
-    2	致命错误
-    7	命令行错误
-    8	内存不足，无法进行操作
-    255	用户已停止进程
-    """
-    # 处理返回码
-    if process.returncode == 0:  # 没有错误
+    if process.returncode == 0: 
         result_class = Result7zip.Success(filepath, password)
-    elif process.returncode == 1:  # 警告（非致命错误，例如被占用）
+    elif process.returncode == 1: 
         result_class = Result7zip.FileOccupied(filepath)
-    elif process.returncode == 2:  # 致命错误
-        stderr = str(process.stderr) + str(process.stdout)  # 错误信息在输出流中
-        # 读取输出流
-        print('【7zip测试信息：', stderr, '】')  # 测试用
-        # if not stderr:  # 处理自解压文件时，返回的stderr流可能为空
-        #     result_class = Result7zip.WrongPassword(filepath)
-        # 备忘录-如何更好的处理自解压文件
+    elif process.returncode == 2:  
+        stderr = str(process.stderr) + str(process.stdout)  
+        print('【7zip测试信息：', stderr, '】')  
         if 'Wrong password' in stderr:
             result_class = Result7zip.WrongPassword(filepath)
         elif 'Missing volume' in stderr:
             result_class = Result7zip.MissingVolume(filepath)
-        elif 'Cannot open the file as' in stderr:  # 备忘录-如果是这个报错可能可以按其指定的文件类型进行测试
+        elif 'Cannot open the file as' in stderr:  
             result_class = Result7zip.NotArchiveOrDamaged(filepath)
-        else:  # 兜底
+        else: 
             result_class = Result7zip.UnknownError(filepath)
-    elif process.returncode == 8:  # 内存不足，无法进行操作
+    elif process.returncode == 8: 
         result_class = Result7zip.NotEnoughSpace(filepath)
-    else:  # 兜底
+    else:  
         result_class = Result7zip.UnknownError(filepath)
 
-    # 处理输出流
+
     archive_info_dict = get_info_from_stdout(process.stdout)
 
     return result_class, archive_info_dict
 
 
 def test_fake_password(file):
-    """使用临时密码测试文件，判断是否进行进一步操作
-    :return: True: 可以使用l命令进行后续测试;
-            False: 无法使用l命令进行后续测试;
-            Result7zip类: 文件本身存在问题.
-    """
+
     result_class, archive_info_dict = call_7zip('l', file, _PASSWORD_FAKE)
-    # 返回密码错误，则该压缩文件已加密且可以使用l命令进行后续测试
     if isinstance(result_class, Result7zip.WrongPassword):
         return True, archive_info_dict
-    # 返回成功，则该压缩文件无法使用l命令进行后续测试（内部文件名未加密或无密码时无法使用l命令测试密码，需要使用t/x）
     elif isinstance(result_class, Result7zip.Success):
         return False, archive_info_dict
-    # 返回其他类型，则该压缩文件本身存在问题，中断后续操作
-    # 返回文件占用/非压缩文件/空间不足/缺失分卷/未知错误，则说明文件本身存在问题
     else:
         return result_class, archive_info_dict
 
 
 class Result7zip:
-    """7zip处理结果
-    7zip Exit Codes
-    0	没有错误
-    1	警告（非致命错误，例如被占用）
-    2	致命错误
-    7	命令行错误
-    8	内存不足，无法进行操作
-    255	用户已停止进程"""
 
     class _Template:
         def __init__(self, file, text, color):
@@ -101,49 +67,41 @@ class Result7zip:
             self.color = color
 
     class Skip(_Template):
-        """跳过（通过filetype库判断后，不通过7zip调用返回结果判断）"""
 
         def __init__(self, file):
             super().__init__(file, '跳过', _COLOR_SKIP)
 
     class WrongPassword(_Template):
-        """密码错误"""
 
         def __init__(self, file):
             super().__init__(file, '密码错误', _COLOR_ERROR)
 
     class MissingVolume(_Template):
-        """缺少分卷"""
 
         def __init__(self, file):
             super().__init__(file, '缺少分卷', _COLOR_ERROR)
 
     class NotArchiveOrDamaged(_Template):
-        """不是压缩文件或文件已经损坏"""
 
         def __init__(self, file):
             super().__init__(file, '不是压缩文件或文件已经损坏', _COLOR_ERROR)
 
     class UnknownError(_Template):
-        """未知错误"""
 
         def __init__(self, file):
             super().__init__(file, '未知错误', _COLOR_ERROR)
 
     class FileOccupied(_Template):
-        """文件被占用"""
 
         def __init__(self, file):
             super().__init__(file, '文件被占用', _COLOR_WARNING)
 
     class NotEnoughSpace(_Template):
-        """磁盘空间不足"""
 
         def __init__(self, file):
             super().__init__(file, '磁盘空间不足', _COLOR_WARNING)
 
     class Success(_Template):
-        """成功"""
 
         def __init__(self, file, password):
             super().__init__(file, '成功', _COLOR_SUCCESS)
@@ -151,7 +109,6 @@ class Result7zip:
 
 
 class Collect7zipResult:
-    """收集7zip的调用结果，并进行统计"""
     _instance = None
     _is_init = False
 
@@ -168,16 +125,13 @@ class Collect7zipResult:
             self._result_dict = dict()  # 结果字典，key为文件路径，value为Result7zip对象
 
     def reset_count(self):
-        """重置计数"""
         self._result_dict.clear()
 
     def collect(self, result_class):
-        """收集结果"""
         file = result_class.file
         self._result_dict[file] = result_class
 
     def get_result_text(self):
-        """获取结果文本"""
         wrong_password = 0  # 密码错误
         missing_volume = 0  # 缺少分卷
         not_archive_or_damaged = 0  # 不是压缩文件或文件已经损坏
@@ -211,20 +165,17 @@ class Collect7zipResult:
 
 
 def get_info_from_stdout(stdout_text: str):
-    """从7zip的stdout中获取相关信息"""
     data_dict = {'filetype': None, 'paths': None}
     if stdout_text:
         text_split = stdout_text.splitlines()
     else:
         return data_dict
 
-    # 提取文件类型
     cut_ = [i for i in text_split if i.startswith('Type = ')]
     if cut_:
         cut_text = [i for i in text_split if i.startswith('Type = ')][0]
         filetype = cut_text[len('Type = '):]
         data_dict['filetype'] = filetype
-    # 提取内部文件路径
     start_index = None
     end_index = None
     for index, i in enumerate(text_split):
